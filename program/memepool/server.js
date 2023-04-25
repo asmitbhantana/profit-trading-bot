ada166b8ae84fbf779c5d8558fd87fc8d186afc4
Code_Bleed <bhantanasmit@gmail.com>
2023-04-25 08:24:56 +0545

    reverse

--- program/memepool/server.js
+++ program/memepool/server.js
@@ -1,22 +1,22 @@
-const express = require("express");
+const express = require('express');
 const {
   isTrackingwallet,
   createNewTransaction,
   updateTransaction,
   havePrevTransaction,
-} = require("../database/action");
-const { Router } = require("../database/model");
+} = require('../database/action');
+const { Router } = require('../database/model');
 const {
   analyzeV2Transaction,
   analyzeV3Transaction,
   analyzeUniversalRouter,
-} = require("./anaylizeTransaction");
+} = require('./anaylizeTransaction');
 
-const path = require("path");
-const { BigNumber } = require("ethers");
+const path = require('path');
+const { BigNumber } = require('ethers');
 
 //connect to the database
-require("../database/connection");
+require('../database/connection');
 
 const app = express();
 
@@ -31,22 +31,22 @@ const callRouter = async (req, res) => {
   const txnData = req.body;
   try {
     if (
-      (txnData.status == "pending" ||
-        txnData.status == "confirmed" ||
-        txnData.status == "failed") &&
+      (txnData.status == 'pending' ||
+        txnData.status == 'confirmed' ||
+        txnData.status == 'failed') &&
       isTrackingwallet(txnData.from)
     ) {
       //if there is no previous txn and only confirmed
       const doesnotHasPrevPendingTxn = await havePrevTransaction(txnData.hash);
       if (
         doesnotHasPrevPendingTxn &&
-        (txnData.status == "confirmed" || txnData.status == "failed")
+        (txnData.status == 'confirmed' || txnData.status == 'failed')
       ) {
         isSmart = true;
-        console.log("smart :) no pending txn, ", txnData.hash);
-        txnData.status = "pending";
+        console.log('smart :) no pending txn, ', txnData.hash);
+        txnData.status = 'pending';
       }
-      const isConfirmed = txnData.status == "confirmed";
+      const isConfirmed = txnData.status == 'confirmed';
 
       const contractCall = txnData;
       const contractCallData = txnData.contractCall;
@@ -55,14 +55,14 @@ const callRouter = async (req, res) => {
       }).exec();
 
       if (!currentRouter) {
-        console.log("Current router failed", txnData);
-        return res.json({ unsuccess: "no-current-router" });
+        console.log('Current router failed', txnData);
+        return res.json({ unsuccess: 'no-current-router' });
       }
 
-      console.log("Current router succeed", txnData);
+      console.log('Current router succeed', txnData);
 
       let routerAddress = contractCall.to;
-      console.log("current router", routerAddress);
+      console.log('current router', routerAddress);
 
       let metadata = {
         txnHash: txnData.hash,
@@ -71,24 +71,24 @@ const callRouter = async (req, res) => {
         to: contractCall.to,
         value: contractCall.value,
         gasLimit: txnData.gas,
-        isConfirmed: txnData.status == "confirmed",
+        isConfirmed: txnData.status == 'confirmed',
         maxFeePerGas: Number(
-          txnData.maxFeePerGas == "" ||
+          txnData.maxFeePerGas == '' ||
             txnData.maxFeePerGas == NaN ||
             txnData.maxFeePerGas == undefined
             ? 0
             : txnData.maxFeePerGas
         ),
         maxPriorityFeePerGas: Number(
-          txnData.maxFeePerGas == "" ||
+          txnData.maxFeePerGas == '' ||
             txnData.maxFeePerGas == NaN ||
             txnData.maxFeePerGas == undefined
             ? 0
             : txnData.maxPriorityFeePerGas
         ),
-        gasUsed: txnData.status == "confirmed" ? txnData.gasUsed : 0,
+        gasUsed: txnData.status == 'confirmed' ? txnData.gasUsed : 0,
         gasFee:
-          txnData.status == "confirmed" &&
+          txnData.status == 'confirmed' &&
           txnData.baseFeePerGas != NaN &&
           txnData.baseFeePerGas != undefined &&
           txnData.maxPriorityFeePerGas != NaN &&
@@ -96,14 +96,14 @@ const callRouter = async (req, res) => {
             ? BigNumber.from(txnData.baseFeePerGas).add(
                 BigNumber.from(txnData.maxPriorityFeePerGas)
               )
-            : BigNumber.from("0"),
+            : BigNumber.from('0'),
       };
       let params = {
         ...contractCallData.params,
         value: contractCall.value,
       };
 
-      if (txnData.status != "failed") {
+      if (txnData.status != 'failed') {
         if (currentRouter.isV3) {
           let subCalls = contractCallData.subCalls;
           analyzeV3Transaction(
@@ -119,7 +119,7 @@ const callRouter = async (req, res) => {
           let commands = contractCallData.params.commands;
           let methodName = contractCallData.methodName;
 
-          console.log("inside of router");
+          console.log('inside of router');
           analyzeUniversalRouter(
             methodName,
             inputs,
@@ -141,10 +141,10 @@ const callRouter = async (req, res) => {
         }
       }
 
-      if (isSmart) {
-        //recall the router for updates
-        callRouter(req, res);
-      }
+      // if (isSmart) {
+      //   //recall the router for updates
+      //   callRouter(req, res);
+      // }
 
       //Save Transaction
       await updateTransaction(metadata.txnHash, {
@@ -154,19 +154,19 @@ const callRouter = async (req, res) => {
         targetMaxGwei: txnData.maxFeePerGas,
         targetMaxPriorityGwei: txnData.maxPriorityFeePerGas,
         targetGasLimit: txnData.gas,
-        flowType: "MempoolTransaction",
+        flowType: 'MempoolTransaction',
       });
 
-      res.json({ success: "txn performing" });
+      res.json({ success: 'txn performing' });
     }
   } catch (err) {
-    console.log("error occured", err);
+    console.log('error occured', err);
   }
 };
 
-app.post("/*", callRouter);
+app.post('/*', callRouter);
 
-app.use("/output", express.static(path.join(__dirname, "../../output/")));
+app.use('/output', express.static(path.join(__dirname, '../../output/')));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
